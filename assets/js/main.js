0f55270862656f812b674c2b18f2daf42591277a
LucaStellato <lucastellato435@gmail.com>
2025-10-15 15:54:48 +0200

    bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,41 +24,41 @@ const formEl = document.getElementById('formEl')
 const nameSurnameField = document.getElementById('name-surname-field')
 const kmField = document.getElementById('km-field')
 const ageField = document.getElementById('age-field')
-const priceFinal = 0;
 const solution = document.getElementById('solution')
 
 //visualization variable for try
 console.log(nameSurnameField, kmField, ageField)
 
 //action to click the submit button
-formEl.addEventListener('submit', (event, priceFinal) => {
+formEl.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log(event)
     //visualization the date insert by the user in consolle
     const nameSurname = nameSurnameField.value;
-    const km = kmField.value;
-    const age = ageField.value;
+    const km = parseFloat(kmField.value);
+    const age = parseInt(ageField.value);
     console.log(nameSurname);
     console.log(km);
     console.log(age);
-   
+
     //constant rate km
     const rate = 0.21
     //operation with calulated price
+  
     const price = kmField.value * rate
+      let priceFinal = parseFloat(price)
     //condition price
-    if (ageField.textContent < 18) {
-        const priceFinal = price * (1 - 20 / 100);
+    if (ageField.textContent >= 65) {
+        priceFinal = price * (1 - 20 / 100);
         console.log(priceFinal.toFixed(2))
-        solution.innerHTML = `<h3>il prezzo finale é di ${priceFinal.toFixed(2)}</h3>`
 
-    } else if (ageField.textContent >= 65) {
-        const priceFinal = price * (1 - 40 / 100);
+
+    } else if (ageField.textContent < 18) {
+        priceFinal = price * (1 - 40 / 100);
         console.log(priceFinal.toFixed(2))
-        solution.innerHTML = `<h3>il prezzo finale é di ${priceFinal.toFixed(2)}</h3>`
-    }else{
-        solution.innerHTML = `<h3>il prezzo finale é di ${priceFinal.toFixed(2)}</h3>`
+
     }
+    solution.innerHTML = `<h3>il prezzo finale é di ${priceFinal.toFixed(2)}</h3>`
 
 
 
