3e51595a552b891b36ed5b01cf0c93dddb098c05
LucaStellato <lucastellato435@gmail.com>
2025-10-15 13:43:14 +0200

    declaration variables in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
-console.log('it work')
+console.log('it work');
+
+const nameSurnameField= document.getElementById('name-surname-field')
+const kmField= document.getElementById('km-field')
+const ageField= document.getElementById('age-field')
