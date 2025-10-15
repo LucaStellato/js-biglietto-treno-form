786756922751d6ebb40f0f09268cac0564bb93eb
LucaStellato <lucastellato435@gmail.com>
2025-10-15 14:08:44 +0200

    milestone 1 complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,21 @@
 console.log('it work');
+const formEl = document.getElementById('formEl')
+const nameSurnameField = document.getElementById('name-surname-field')
+const kmField = document.getElementById('km-field')
+const ageField = document.getElementById('age-field')
 
-const nameSurnameField= document.getElementById('name-surname-field')
-const kmField= document.getElementById('km-field')
-const ageField= document.getElementById('age-field')
+console.log(nameSurnameField, kmField, ageField)
+
+formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event)
+
+    const nameSurname = nameSurnameField.value;
+    const km = kmField.value;
+    const age = ageField.value;
+
+    console.log(nameSurname);
+    console.log(km);
+    console.log(age);
+})
+
