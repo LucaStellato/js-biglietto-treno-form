221f7d276e3116f684a35ee55c5b43ca06c6ac0a
LucaStellato <lucastellato435@gmail.com>
2025-10-15 15:00:07 +0200

    add comment to js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,14 @@
-console.log('it work');
-const formEl = document.getElementById('formEl')
-const nameSurnameField = document.getElementById('name-surname-field')
-const kmField = document.getElementById('km-field')
-const ageField = document.getElementById('age-field')
-const priceFinal = 0;
-const solution = document.getElementById('solution')
-//Milestone-1
-console.log(nameSurnameField, kmField, ageField)
+/*Milestone 1. 
+Iniziamo implementando il programma senza alcuna estetica: 
+usando esclusivamente due input e un bottone (non stilizzati), 
+realizziamo le specifiche scritte sopra. 
+La risposta finale (o output) sarà anch’essa da scrivere in console.*/
 
-formEl.addEventListener('submit', (event, priceFinal) => {
-    event.preventDefault();
-    console.log(event)
 
-    const nameSurname = nameSurnameField.value;
-    const km = kmField.value;
-    const age = ageField.value;
-    console.log(nameSurname);
-    console.log(km);
-    console.log(age);
-    //Milestone 1
-
-    /*MILESTONE 2:
+/*MILESTONE 2:
 Solo una volta che il milestone 1 sarà completo e funzionante allora 
 realizzeremo un form in pagina in cui l’utente potrà inserire i dati
- e visualizzare il calcolo finale con il prezzo. 
+e visualizzare il calcolo finale con il prezzo. 
 Il recap dei dati e l'output del prezzo finale, 
 andranno quindi stampati in pagina 
 (il prezzo dovrà essere formattato con massimo due decimali, 
@@ -31,14 +16,37 @@ per indicare i centesimi sul prezzo).
 Questo richiederà un minimo di ricerca.
 */
 
+//try
+console.log('it work');
 
+//Declaration variable to js
+const formEl = document.getElementById('formEl')
+const nameSurnameField = document.getElementById('name-surname-field')
+const kmField = document.getElementById('km-field')
+const ageField = document.getElementById('age-field')
+const priceFinal = 0;
+const solution = document.getElementById('solution')
 
+//visualization variable for try
+console.log(nameSurnameField, kmField, ageField)
 
+//action to click the submit button
+formEl.addEventListener('submit', (event, priceFinal) => {
+    event.preventDefault();
+    console.log(event)
+    //visualization the date insert by the user in consolle
+    const nameSurname = nameSurnameField.value;
+    const km = kmField.value;
+    const age = ageField.value;
+    console.log(nameSurname);
+    console.log(km);
+    console.log(age);
+   
+    //constant rate km
     const rate = 0.21
-
+    //operation with calulated price
     const price = kmField.value * rate
-
-
+    //condition price
     if (ageField.textContent < 18) {
         const priceFinal = price * (1 - 20 / 100);
         console.log(priceFinal.toFixed(2))
@@ -47,6 +55,8 @@ Questo richiederà un minimo di ricerca.
     } else if (ageField.textContent >= 65) {
         const priceFinal = price * (1 - 40 / 100);
         console.log(priceFinal.toFixed(2))
+        solution.innerHTML = `<h3>il prezzo finale é di ${priceFinal.toFixed(2)}</h3>`
+    }else{
         solution.innerHTML = `<h3>il prezzo finale é di ${priceFinal.toFixed(2)}</h3>`
     }
 
